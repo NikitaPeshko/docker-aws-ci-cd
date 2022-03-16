18ff96f0179e4e104ad110b8aa54ea3212bd27bd
NikitaPeshko <79310641+NikitaPeshko@users.noreply.github.com>
2022-03-16 11:16:02 +0300

    temp

---
+++ temp2/src/main/java/fisrt.java
@@ -0,0 +1,8 @@
+import com.epam.Hello;
+
+public class fisrt {
+    public static void main(String[] args) {
+        Hello hello=new Hello("dcnbjsdcs");
+        System.out.println("dsvbsdfns");
+    }
+}
