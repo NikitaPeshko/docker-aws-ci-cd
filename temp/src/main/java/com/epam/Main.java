1cd6950df0987bb8b9657756a0e7db061a10d24e
NikitaPeshko <79310641+NikitaPeshko@users.noreply.github.com>
2022-03-16 11:50:26 +0300

    hotfix 5

--- temp/src/main/java/com/epam/Main.java
+++ temp/src/main/java/com/epam/Main.java
@@ -35,6 +35,10 @@ public class Main {
         System.out.println("dvbxncbvxncbv");
         System.out.println("dvbxncbvxncbv");
         System.out.println("dvbxncbvxncbv");
+        System.out.println("dvbxncbvxncbv");
+        System.out.println("dvbxncbvxncbv");
+        System.out.println("dvbxncbvxncbv");
+        System.out.println("dvbxncbvxncbv");
 
 
 
